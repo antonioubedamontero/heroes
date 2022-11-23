c5ded48b4952e7cea45a90b02bceadb506f44225
Antonio Úbeda Montero <anubedam@gmail.com>
2022-11-23 07:46:24 +0100

    material and flex layout module added

---
+++ src/app/base/material/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table'; 
+import { MatPaginatorModule } from '@angular/material/paginator'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
+import { MatDialogModule } from '@angular/material/dialog'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatChipsModule } from '@angular/material/chips'; 
+import { MatDividerModule } from '@angular/material/divider'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar'; 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule { }
