5608b03788967644783fa07a8f8e6eaf76e66d43
Antonio Úbeda Montero <anubedam@gmail.com>
2022-11-25 20:57:21 +0100

    add hero filter with tests

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeroFilterComponent } from './hero-filter/hero-filter.component';
+import { BaseModule } from '../base/base.module';
+4
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroFilterComponent
+  ],
   imports: [
+    BaseModule,
     CommonModule
+  ],
+  exports: [
+    HeroFilterComponent
   ]
 })
 export class SharedModule { }
